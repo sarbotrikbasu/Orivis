03d1365905473e8d4d882f8950ae8d9b5ba6b45c
sarbotrikbasu <sarbo.idfcsummer@gmail.com>
2026-06-21 17:03:59 +0530

    Change default MT5 login credentials and server
    Updated default login credentials and server address.

--- config.py
+++ config.py
@@ -40,9 +40,9 @@ def parse_comma_list(env_name, default=""):
     return [item.strip() for item in value.split(",") if item.strip()]
 
 
-DEFAULT_LOGIN = get_env("MT5_LOGIN", "433533897")
-DEFAULT_PASSWORD = get_env("MT5_PASSWORD", "Sarbo1998@")
-DEFAULT_SERVER = get_env("MT5_SERVER", "Exness-MT5Trial7")
+DEFAULT_LOGIN = get_env("MT5_LOGIN", "128074421")
+DEFAULT_PASSWORD = get_env("MT5_PASSWORD", "Pradip1999@")
+DEFAULT_SERVER = get_env("MT5_SERVER", "Exness-MT5Real7")
 
 FIB_GEN_SYMBOLS = parse_comma_list(
     "FIB_GEN_SYMBOLS",
@@ -58,12 +58,3 @@ TIMEFRAMES = parse_comma_list("TIMEFRAMES", "5m,15m,1h,1d")
 
 FIB_KEYS = ["Fib1", "Fib2", "Fib3", "Fib4", "Fib5"]
 
-FIB_GEN_API_URL       = get_env("FIB_GEN_API_URL",       "http://74.208.190.247:8001/signal")
-FIB_JPY_API_URL       = get_env("FIB_JPY_API_URL",       "http://74.208.190.247:8002/signal")
-RSI_GEN_API_URL       = get_env("RSI_GEN_API_URL",       "http://74.208.190.247:8003")
-RSI_JPY_API_URL       = get_env("RSI_JPY_API_URL",       "http://74.208.190.247:8004")
-BOLLINGER_GEN_API_URL = get_env("BOLLINGER_GEN_API_URL", "http://74.208.190.247:8005/bollinger")
-BOLLINGER_JPY_API_URL = get_env("BOLLINGER_JPY_API_URL", "http://74.208.190.247:8006/bollinger")
-MA_GEN_API_URL        = get_env("MA_GEN_API_URL",        "http://74.208.190.247:8007/signals")
-MA_JPY_API_URL        = get_env("MA_JPY_API_URL",        "http://74.208.190.247:8008/signals")
-COMBINED_API_URL      = get_env("COMBINED_API_URL",      "http://74.208.190.247:8000")
